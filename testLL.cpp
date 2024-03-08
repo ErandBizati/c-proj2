acfcb3f6b54e1065b446766b1715cdb067ccfa73
Erand Bizati <89628769+ErandBizati@users.noreply.github.com>
2024-03-08 00:01:46 -0500

    Update testLL.cpp

--- testLL.cpp
+++ testLL.cpp
@@ -1,7 +1,33 @@
+/************************************************************
+* Author: Erand Bizati
+* Major: Comp Sci
+* Due Date: 3/8/24
+* Course: CSC237
+* Professor Name: Dr. Spiegel
+* Project: 2
+* Filename: testLL.cpp
+************************************************************/
+
 #include "CLinkedList.h"
 #include <iostream>
 #include <limits>
 
+
+
+/*********************************************************************************************************
+ * Function: main
+ * -------------------------------------------------------------------------------------------------------
+ * Description: Serves as the entry point for the program. It provides a test driver for the `CLinkedList` 
+ * class, allowing the user to perform operations such as insert, remove, print forwards, and print 
+ * backwards on a circular linked list of integers.
+ * -------------------------------------------------------------------------------------------------------
+ * Parameters: None
+ * -------------------------------------------------------------------------------------------------------
+ * Returns: int - The exit status of the program.
+ * -------------------------------------------------------------------------------------------------------
+ * Pre-conditions: None
+ * Post-conditions: Executes a series of list operations based on user input until the exit option is chosen.
+ *********************************************************************************************************/
 int main() {
     CLinkedList<int> list;
     char choice;
@@ -14,8 +40,8 @@ int main() {
             case 'I':
                 std::cout << "Enter an integer to insert: ";
                 if (!(std::cin >> value)) {
-                    std::cin.clear(); // Clears the error flag
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignores the rest of the input
+                    std::cin.clear(); 
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
                     std::cout << "Invalid input. Please enter an integer.\n";
                     break;
                 }
@@ -24,8 +50,8 @@ int main() {
             case 'R':
                 std::cout << "Enter an integer to remove: ";
                 if (!(std::cin >> value)) {
-                    std::cin.clear(); // Clears the error flag
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignores the rest of the input
+                    std::cin.clear(); 
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
                     std::cout << "Invalid input. Please enter an integer.\n";
                     break;
                 }
@@ -40,7 +66,7 @@ int main() {
                 list.printBackward();
                 break;
             case 'E':
-                std::cout << "Exiting...\n";
+                std::cout << "Exiting\n";
                 break;
             default:
                 std::cout << "Invalid option.\n";
